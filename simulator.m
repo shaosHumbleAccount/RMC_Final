d65423a1e32ccd70d4809e0e0221d64dea2ede87
Shao <tianlan.shao@tum.de>
2013-07-07 14:38:57 +0200

    correct M and C in getqpp

--- simulator.m
+++ simulator.m
@@ -1,5 +1,4 @@
 q_0 = [0.3; 0; 0; 0; 0; 0];
-%q_0 = 0;
 qp_0 = zeros(6,1);
 qpp_0 = zeros(6,1);
 
@@ -15,8 +14,8 @@ B = zeros(1,6);
 Wf = 1;
 
 
-delta_t = 0.01; % in second
-max_t = 2; % in second
+delta_t = 0.0001; % in second
+max_t = 1; % in second
 
 t_s = 0 : delta_t : max_t;
 q_s = zeros(length(t_s),6);
@@ -29,27 +28,29 @@ desired_qp_s = zeros(length(t_s),6);
 %[q_t qp_t] = trajectory_fourier(A, B, 0, Wf, q_0);%start at desired q(t = 0)
 
 for t_idx = 1:length(t_s)
+    tic
     t = t_s(t_idx);
     [desired_q, desired_qp] = trajectory_fourier(A, B, t, Wf, q_0);
     
     desired_q_s(t_idx,:) = desired_q;
     desired_qp_s(t_idx,:) = desired_qp;
     
+    q_t = q_t + qp_t.*delta_t;
+    qp_t = qp_t + qpp_t.*delta_t;
     qpp_t = getqpp(q_t, qp_t, desired_q, desired_qp);
-    qp_t = qp_t + qpp_t*delta_t;
     
-    q_t = q_t + qp_t*delta_t;
     
     q_s(t_idx,:) = q_t;
     qp_s(t_idx,:) = qp_t;
     qpp_s(t_idx,:) = qpp_t;
     
-    
+    toc
 end
 
 figure
 hold on
 plot(q_s(:,1),'r')
+plot(q_s(:,2:end))
 plot(desired_q_s(:,1),'g')
 
 
